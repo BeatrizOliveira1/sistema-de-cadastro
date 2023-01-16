f82e86bf846681519278091cae412ee0a7a0a712
BeatrizOliveira1 <106554429+BeatrizOliveira1@users.noreply.github.com>
2023-01-16 19:43:36 -0300

    corrigindo o app js

--- app.js
+++ app.js
@@ -1,4 +1,1 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("aplicação está se conectado ao banco de dados")
-// mais um console log de teste:
-console.log("A Bea frz esse commit sem voce ver na aula!")
